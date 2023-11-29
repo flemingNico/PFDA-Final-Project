cf0831f85b0db10a8a11d044668f28e35cd0d53e
Nico Fleming <nicolerobinsonfleming@gmail.com>
2023-11-29 10:41:21 -0600

    added pink_1 and began pink_2

--- src/project.py
+++ src/project.py
@@ -8,34 +8,19 @@ def small_heart(screen): #feature 1
     pink_4 = pygame.Color(222, 0, 81)
     pink_5 = pygame.Color(145, 0, 53)
     white = pygame.Color(255, 255, 255)
-#first pillar for N
-    surf = pygame.Surface((20, 100))
+#pink_1 upper square
+    surf = pygame.Surface((40, 40))
     surf.fill(pink_1)
     screen.blit(surf, (300, 200))
-#left most middle slash for N
-    surf = pygame.Surface((20, 25))
+#pink_1 lower square
+    surf = pygame.Surface((40, 40))
+    surf.fill(pink_1)
+    screen.blit(surf, (280, 220))
+#pink_2 top right square
+    surf = pygame.Surface((20, 20))
     surf.fill(pink_2)
-    screen.blit(surf, (320, 215))
-#middle most middle slash for N
-    surf = pygame.Surface((20, 25))
-    surf.fill(pink_3)
-    screen.blit(surf, (340, 238))
-#right most middle slash for N
-    surf = pygame.Surface((20, 25))
-    surf.fill(pink_4)
-    screen.blit(surf, (355, 260))
-#second pillar for N/ pillar for F
-    surf = pygame.Surface((20, 100))
-    surf.fill(pink_5)
-    screen.blit(surf, (375, 200))
-#top stroke of F
-    surf = pygame.Surface((90, 20))
-    surf.fill(white)
-    screen.blit(surf, (375, 200))
-#middle stroke of F
-    surf = pygame.Surface((50, 20))
-    surf.fill(white)
-    screen.blit(surf, (375, 238))
+    screen.blit(surf, (360, 200))
+
 
 
 def big_heart(screen): #feature 2
